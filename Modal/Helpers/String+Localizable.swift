e13d853287888105043706660ffb4761b41a2f6e
Bruno Alves <oemaildobrunoalves@gmail.com>
2020-08-20 19:02:56 -0300

    Localizable

---
+++ Modal/Helpers/String+Localizable.swift
@@ -0,0 +1,17 @@
+//
+//  String+Localizable.swift
+//  Modal
+//
+//  Created by Bruno Alves on 20/08/20.
+//  Copyright © 2020 Bruno Alves. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    
+    static func localize(_ key: String) -> String {
+        NSLocalizedString(key, comment: "")
+    }
+    
+}
