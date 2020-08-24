a0da0ee77bdadf0b0e63bc35bf33cd63ab049b1e
Bruno Alves <oemaildobrunoalves@gmail.com>
2020-08-24 05:53:45 -0300

    add carthage

---
+++ Modal/Modules/FilterViewController/FilterTableViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  FilterTableViewCell.swift
+//  Modal
+//
+//  Created by Bruno Alves on 24/08/20.
+//  Copyright © 2020 Bruno Alves. All rights reserved.
+//
+
+import UIKit
+
+class FilterTableViewCell: UITableViewCell {
+}
